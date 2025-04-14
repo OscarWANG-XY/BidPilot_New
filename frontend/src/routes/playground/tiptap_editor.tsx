f480ecf7a82fae3fc47a72d0c37686f01bc3645a
OscarWANG-XY <hui.wang1986@gmail.com>
2025-04-14 23:38:19 +0800

    [TiptapEditor_pro] 从头开始：基础框架

--- frontend/src/routes/playground/tiptap_editor.tsx
+++ frontend/src/routes/playground/tiptap_editor.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
 import TiptapEditorTest from '@/components/TiptapEditor/TE_test';
+import TiptapEditor from '@/components/TiptapEditor/TiptapEditor_pro';
 
 export const Route = createFileRoute('/playground/tiptap_editor')({
   component: RouteComponent,
@@ -9,7 +10,8 @@ function RouteComponent() {
   return(
   <>
     <div>Hello "/playground/tiptap_editor"!</div>
-    <TiptapEditorTest />
+    {/* <TiptapEditorTest /> */}
+    <TiptapEditor />
   </>
 
   )
