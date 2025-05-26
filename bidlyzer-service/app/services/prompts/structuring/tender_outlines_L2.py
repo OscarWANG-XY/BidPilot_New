84d1612bc2e282c27fb786ae0094e9baaedab3ec
OscarWANG-XY <hui.wang1986@gmail.com>
2025-05-26 13:22:40 +0800

    [bidlyzer - app - services - prompts] 完成了prompts的迁移， tender_outlines_L1 完成测试，其他待测试

---
+++ bidlyzer-service/app/services/prompts/structuring/tender_outlines_L2.py
@@ -0,0 +1,249 @@
+import os
+from typing import List, Dict, Tuple, Any, Optional
+import logging
+logger = logging.getLogger(__name__)
+
+from app.services.llm.llm_models import LLMConfigModel
+from langchain_core.prompts import ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate
+from app.services.task_service import count_tokens
+
+
+# 目标 -> 准备好以下内容
+# context, instruction(要解决的问题, 包括output_format要求), supplemnt相关补充  (对用户可见)
+# prompt Template 将 context, instruction, supplemnt 组织到一块  （对用户不可见）。  
+# llm_config 配置模型参数 （对用户不可见）
+
+
+class TenderOutlinesL2PromptBuilder():
+
+    """ 
+    文档二、三级大纲分析的prompt builder
+    
+    目标任务：利用大模型，从DOC文档里 识别 最高层级 的标题
+
+    输入： doc:tiptapJSON
+    输出： prompt_config: Dict(str, Any), task_input:List[Dict], meta:Dict(str, Any);
+    附带： 模拟的prompt： raw_prompt, formatted_prompt
+
+    大模型调用： 多个并发调用
+    
+    """
+
+    def __init__(self, doc: Any):
+
+        # 输入参数
+        self.doc = doc
+
+        # 模型配置
+        self.llm_config = self._build_llm_config().to_model()
+
+        # 定义角色和prompt模板
+        self.system_role = self._define_system_role()
+        self.prompt_template = self._build_prompt_template()
+
+
+        # 用户指令
+        self.indexed_chapters,_ = self._prepare_context()    # key_payload
+        self.instruction = self._prepare_instruction()
+        self.supplement = self._prepare_supplement()
+        self.output_format = self._prepare_output_format()
+
+        # meta
+        _,self.index_path_map = self._prepare_context()
+        self.chapters_tokens = sum([count_tokens(indexed_chapter["paragraphs"]) for indexed_chapter in self.indexed_chapters])
+        self.instruction_tokens = count_tokens(self.instruction)
+        self.supplement_tokens = count_tokens(self.supplement)
+        self.output_format_tokens = count_tokens(self.output_format)
+        self.prompt_template_tokens = count_tokens(self.prompt_template)
+        self.in_tokens = self.chapters_tokens + self.instruction_tokens + self.supplement_tokens + self.output_format_tokens + self.prompt_template_tokens
+
+    def output_params(self) -> Tuple[Dict[str,any], List[Dict[str,any]], Dict[str,any]]:
+        # 多任务主要通过task_inputs 来体现
+
+        prompt_config = {
+            "llm_config": self.llm_config,
+            "prompt_template": self.prompt_template,
+            "system_role": self.system_role,
+        }
+
+        task_inputs = []
+        for chapter in self.indexed_chapters:
+            task_params = {
+                "context": chapter["paragraphs"],
+                "instruction": self.instruction,
+                "supplement": self.supplement,
+                "output_format": self.output_format,
+            }
+            task_inputs.append(task_params)
+
+        meta = {
+            "index_path_map": self.index_path_map,
+            "in_tokens" : self.in_tokens,
+            "chapters_tokens": self.chapters_tokens,
+            "instruction_tokens": self.instruction_tokens,
+            "supplment_tokens":self.supplement_tokens,
+            "output_format_tokens": self.output_format_tokens,
+            "prompt_template_tokens": self.prompt_template_tokens
+        }
+
+        return prompt_config, task_inputs, meta
+    
+#    def simulate_prompt(self) -> str:
+    def simulate_prompt(self) -> Tuple[List[Dict[str,Any]], List[Dict[str,Any]]]:
+        """
+        模拟生成完整的 prompt
+        
+        Args:
+            context: 输入的上下文信息
+            
+        Returns:
+            str: 完整的 prompt 内容
+        """
+        # 创建聊天提示模板
+        prompt = ChatPromptTemplate.from_messages([
+            SystemMessagePromptTemplate.from_template(
+                self.system_role
+            ),
+            HumanMessagePromptTemplate.from_template(
+                # self.task.prompt_template,
+                self.prompt_template,
+                input_variables=[
+                    "context", 
+                    "instruction"
+                    "supplement"
+                    "output_format"
+                    ]
+            )
+        ])
+        
+        # 格式化模板
+        raw_prompts = []
+        formatted_prompts = []
+        for indexed_chapter in self.indexed_chapters:
+            raw_prompt = prompt.format_messages(
+                context=indexed_chapter["paragraphs"],
+                instruction=self.instruction,
+                supplement=self.supplement,
+                output_format=self.output_format
+            )
+
+            # 转换为易读的格式
+            formatted_prompt = [
+                {
+                    "role": message.type,
+                    "content": message.content
+                }
+                for message in raw_prompt
+            ]
+            raw_prompts.append(raw_prompt)
+            formatted_prompts.append(formatted_prompt)
+        
+        return raw_prompts, formatted_prompts
+
+
+    def _define_system_role(self) -> str:
+
+        return"""
+
+你是招投标领域的投标文件编写专家。
+
+"""
+
+
+    def _prepare_instruction(self) -> str:
+
+        # 1）以下提取标题，不做层级限定，实际测试过程中，“最多三个层级” 或者 “最多两个层级” 的限定，都会导致输出结果不准确。
+        # 2）第4点，避免了附件章节开头的目录和附件标题的重复识别，同时又确保带有附件的章节，附件标题被识别。
+        # 3）正文的表述很重要，但可能会将章节末尾的附件排除在外，所以第4点作为补充很重要。 
+
+
+
+        return """
+我会提供招标文件提取的文档片段（材料A），
+文档片段可能是文档开头的封面和目录，也可能是文档的章节片段。
+
+请忽略封面或目录，对于章节片段请完成以下任务：
+1. 识别正文中的标题
+2. 请注意区分章节开头的目录和正文的标题，不要将目录的标题作为正文的标题。
+3. 请注意区分列表和正文的标题，不要将列表项作为正文的标题。
+4. 请注意包含章节末尾的附件标题，但忽略章节开头的附件标题目录。
+"""
+
+
+    def _prepare_context(self) -> Tuple[List[str], Dict[str, str]]:
+        """
+        准备请求数据
+        """ 
+
+        from app.clients.tiptap.helpers import TiptapUtils
+        indexed_chapters, index_path_map = TiptapUtils.extract_chapters(
+            doc = self.doc, 
+            max_length = None,
+            )
+
+        return indexed_chapters, index_path_map
+
+
+    def _prepare_supplement(self) -> str:
+        """
+        准备补充 (对用户可见和修改) 
+        """
+
+        return "此任务无补充内容"
+    
+
+
+    def _prepare_output_format(self) -> str:
+        return """
+        
+- 只输出符合JSON格式的数据，不要添加解释、注释或 Markdown 标记。
+- 示例：
+[
+    {"index": int, "level": int, "title": str}, 
+    {"index": int, "level": int, "title": str}
+]
+- 一个标题一条数据， 如果只有一个层级的标题，则只输出一个层级。
+- 如未识别到标题，返回空列表。
+
+"""
+
+
+    def _build_prompt_template(self) -> str:
+            return """
+你将执行以下任务：
+
+【任务目标】
+{instruction}
+
+【输出格式】
+{output_format}
+
+以下是你将使用的内容：
+
+【材料A：主要上下文】
+{context}
+
+【材料B：补充信息】
+{supplement}
+
+请严格根据材料A和B完成任务。
+
+"""
+
+
+    def _build_llm_config(self) -> LLMConfigModel:
+        """构建LLM配置， temperature = 0.2 和 top_p = 0.6, qwen-max-0125 模型 有较为稳定的输出"""
+        return LLMConfigModel(
+                    llm_model_name = "qwen-max-0125",  
+                    temperature = 0.2,
+                    top_p =  0.6,
+                    streaming = True,
+                    api_key = os.getenv("ALIBABA_API_KEY"),
+                    base_url = "https://dashscope.aliyuncs.com/compatible-mode/v1",
+                    max_workers = 4,
+                    timeout = 60,
+                    retry_times = 3
+                )
+    
+
+
---
+++ bidlyzer-service/app/services/prompts/structuring/tender_outlines_tables.py
@@ -0,0 +1,201 @@
+import os
+from typing import List, Dict, Tuple, Any, Optional
+import logging
+logger = logging.getLogger(__name__)
+
+from app.services.llm.llm_models import LLMConfigModel
+from langchain_core.prompts import ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate
+from app.services.task_service import count_tokens
+
+
+# 目标 -> 准备好以下内容
+# context, instruction(要解决的问题, 包括output_format要求), supplemnt相关补充  (对用户可见)
+# prompt Template 将 context, instruction, supplemnt 组织到一块  （对用户不可见）。  
+# llm_config 配置模型参数 （对用户不可见）
+
+
+class TenderOutlinesTables():
+    """文档大纲分析器，用于提取OutlineL1"""
+
+    def __init__(self, data_input: Any):
+        # 类的传参都一定会经过__init__方法， 基本它写在类后面的（）里。  
+        # 想让对象记住一个变量，都需要在变量前加self. 
+        self.data_input = data_input
+        self.tables, self.index_path_map = self._prepare_context()
+        self.instruction = self._prepare_instruction()
+        self.supplement = self._prepare_supplement()
+        self.output_format = self._prepare_output_format()
+        self.prompt_template = self._build_prompt_template()
+        self.llm_config = self._build_llm_config().to_model()
+
+        self.tables_tokens = sum([count_tokens(table["markdown"]) for table in self.tables])
+        self.instruction_tokens = count_tokens(self.instruction)
+        self.supplement_tokens = count_tokens(self.supplement)
+        self.output_format_tokens = count_tokens(self.output_format)
+        self.prompt_template_tokens = count_tokens(self.prompt_template)
+        self.in_tokens = self.tables_tokens + self.instruction_tokens + self.supplement_tokens + self.output_format_tokens + self.prompt_template_tokens
+
+    def output_params(self) -> Tuple[Dict[str,any], List[Dict[str,any]], Dict[str,any]]:
+
+        model_params = {
+            "llm_config": self.llm_config,
+            "prompt_template": self.prompt_template,
+        }
+
+        tasks = []
+        for table in self.tables:
+            task_params = {
+                "context": table["markdown"],
+                "instruction": self.instruction,
+                "supplement": self.supplement,
+                "output_format": self.output_format,
+            }
+            tasks.append(task_params)
+
+        meta = {
+            "index_path_map": self.index_path_map,
+            "in_tokens" : self.in_tokens,
+            "tables_tokens": self.tables_tokens,
+            "instruction_tokens": self.instruction_tokens,
+            "supplment_tokens":self.supplement_tokens,
+            "output_format_tokens": self.output_format_tokens,
+            "prompt_template_tokens": self.prompt_template_tokens
+        }
+
+        return model_params, tasks, meta
+
+    def _prepare_context(self) -> Tuple[List[str], Dict[str, str]]:
+        """
+        准备请求数据
+        """ 
+
+        from app.clients.tiptap.helpers import TiptapUtils
+
+        tables, index_path_map = TiptapUtils.extract_tables_to_markdown(self.data_input)
+
+        return tables, index_path_map
+
+
+    def _prepare_supplement(self) -> str:
+        """
+        准备补充 (对用户可见和修改) ===
+        """
+
+        return "此任务无补充内容"
+    
+
+    def _prepare_instruction(self) -> str:
+
+        return """
+我提供了表格的markdown文本（材料A），每个表格有一个index（位置索引）。
+任务要求：
+1. 请根据表格的内容，制定表格的caption。
+2. 请将我提供的markdown文本视为完整的一个表格，不要将表格拆分。
+
+"""
+
+
+
+    def _prepare_output_format(self) -> str:
+        return """
+        
+- 只输出符合JSON格式的数据，不要添加解释、注释或 Markdown 标记。
+- 示例：
+[
+    {"index": int, "caption": str}
+]
+- 如果表格为空，则返回空列表。
+
+"""
+
+
+    def _build_prompt_template(self) -> str:
+            return """
+你将执行以下任务：
+
+【任务目标】
+{instruction}
+
+【输出格式】
+{output_format}
+
+以下是你将使用的内容：
+
+【材料A：主要上下文】
+{context}
+
+【材料B：补充信息】
+{supplement}
+
+请严格根据材料A和B完成任务。
+
+"""
+
+
+    def _build_llm_config(self) -> LLMConfigModel:
+        """构建LLM配置， temperature = 0.2 和 top_p = 0.6, qwen-max-0125 模型 有较为稳定的输出"""
+        return LLMConfigModel(
+                    llm_model_name = "qwen-max-0125",  
+                    temperature = 0.2,
+                    top_p =  0.6,
+                    streaming = True,
+                    api_key = os.getenv("ALIBABA_API_KEY"),
+                    base_url = "https://dashscope.aliyuncs.com/compatible-mode/v1",
+                    max_workers = 4,
+                    timeout = 60,
+                    retry_times = 3
+                )
+    
+
+#    def simulate_prompt(self) -> str:
+    def simulate_prompt(self) -> Tuple[str, List[Dict[str, Any]]]:
+        """
+        模拟生成完整的 prompt
+        
+        Args:
+            context: 输入的上下文信息
+            
+        Returns:
+            str: 完整的 prompt 内容
+        """
+        # 创建聊天提示模板
+        prompt = ChatPromptTemplate.from_messages([
+            SystemMessagePromptTemplate.from_template(
+                "你是一个专业的招标文档分析助手，帮助用户分析文档的结构和内容。"
+            ),
+            HumanMessagePromptTemplate.from_template(
+                # self.task.prompt_template,
+                self.prompt_template,
+                input_variables=[
+                    "context", 
+                    "instruction"
+                    "supplement"
+                    "output_format"
+                    ]
+            )
+        ])
+        
+        # 格式化模板
+        simulated_prompt_set = []
+        formatted_prompt_set = []
+        for table in self.tables:
+            simulated_prompt = prompt.format_messages(
+                context=table["markdown"],
+                instruction=self.instruction,
+                supplement=self.supplement,
+                output_format=self.output_format
+
+            )
+
+            # 转换为易读的格式
+            formatted_prompt = [
+                {
+                    "role": message.type,
+                    "content": message.content
+                }
+                for message in simulated_prompt
+            ]
+            simulated_prompt_set.append(simulated_prompt)
+            formatted_prompt_set.append(formatted_prompt)
+        
+        return simulated_prompt_set, formatted_prompt_set
